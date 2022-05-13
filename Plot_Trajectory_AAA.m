bf9663cc2604284b57f1bfa8183843017730efba
99Abdel <a.msaad@studenti.unibs.it>
2022-05-13 17:36:13 +0200

    vabene

---
+++ Plot_Trajectory_AAA.m
@@ -0,0 +1,35 @@
+function Plot_Trajectory_AAA(Qi,Qf,L,X,Y,Z,n,titolo,T)
+% aggiungere un nono elemento di tempo per avere il plot animato di durata
+% T
+
+    Argomenti = 8;
+       
+    figure
+    hold on
+    Plot_AAA(Qi,L,"xyz")
+    
+    if nargin > Argomenti
+        dt = T/n;
+        
+        for i = 1:n
+            plot3(X(i),Y(i),Z(i),'->m')
+            pause(dt)
+        end
+         
+    else
+         plot3(X,Y,Z,'->m')
+    end
+    
+    Plot_AAA(Qf,L,"xyz")
+    title(titolo)
+    
+    grid on
+    axis equal
+    pbaspect([20 20 20])
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    hold off
+    
+end
+
