f4f07d9c7113e7695940c51c227d8ba954bd4f05
Zenorhk <andreazanetti11@gmail.com>
2022-05-20 16:43:55 +0200

    HotHell dynamics

--- Test_Kin_Dyn_Dir_Inv_AAA.m
+++ Test_Kin_Dyn_Dir_Inv_AAA.m
@@ -4,6 +4,11 @@ clear all
 close all
 clc
 
+
+%% ------------------------------------------------------------------------------
+%  ---------------------------- KINEMATICS DATA ---------------------------------
+%  ------------------------------------------------------------------------------
+
 l1 = 10;   % link 1
 l2 = 5;    % link 2
 l3 = 2;    % link 3
@@ -11,13 +16,13 @@ l3 = 2;    % link 3
 L = [l1; l2; l3];    % matrice link
 
 
-%-------- Direct cinematic configuration ----------
+%-------- Direct kinematic configuration ----------
 
 Qi = [0, -pi/2, 0]';
 Qf = [0, 0, pi/2]';
 
 
-%-------- Inverse cinematic configuration ----------
+%-------- Inverse kinematic configuration ----------
 
 Qini_Inv = [0.1 -pi/8 -0.5*pi];
 Si = [-2 -4 12];
@@ -31,7 +36,7 @@ dQ = Qf-Qi;
 dS = Sf-Si;
 
 
-%-------- Direct cinematic angular Speed Matrixes ----------
+%-------- Direct kinematic angular Speed Matrixes ----------
 
 Lry= [0, 0, 1, 0;
       0, 0, 0, 0;
@@ -44,157 +49,9 @@ Lrz=[0,-1, 0, 0;
      0, 0, 0, 0];
 
  
-%% Transizione Velocità da giunti angolari a cartesiano
-
-S = [];
-Sp = [];
-Spp = [];
-
-
-for i = 1:n
-    
-    
-    % ---------------------------------------------------------------------
-    % ----------------------- DIRECT KINEMATICS ---------------------------
-    
-    t=(i-1)*T/(n-1);      % time from 0 to T with step dT
-    tt(i)=t;
-    [a(i),ap(i),app(i)] = cycloidLaw(t,T,Qi(1),dQ(1));
-    [b(i),bp(i),bpp(i)] = cycloidLaw(t,T,Qi(2),dQ(2));
-    [c(i),cp(i),cpp(i)] = cycloidLaw(t,T,Qi(3),dQ(3));
-    
-    q = [a(i);b(i);c(i)];
-    qp = [ap(i);bp(i);cp(i)];
-    qpp = [app(i);bpp(i);cpp(i)];
-    
-    [s,M01,M12,M23] = Rotation_Matrixes_AAA(q,L); 
-    
-    S = [S s];
-    
-    M02 = M01*M12;
-    M03 = M02*M23;
-    
-    
-    % -------------------------------------IMPORTANTE--------------------------------
-    % -------------------------------------------------------------------------------
-    
-    % In generale Hij_k significa accelerazione di (j) rispetto ad (i) vista dal sistema 
-    % (k)  (in maniera analoga anche per le velocità)
-    
-    % -------------------------------------IMPORTANTE--------------------------------
-    % -------------------------------------------------------------------------------
-    
-    
-    % ------------ SPEED EVALUATION -------------
-    
-    w01_0 = Lrz*qp(1);      %speed in 0-1 seen by the referece system 0
-    w12_0 = M01*Lry*qp(2)*inv(M01);     %speed in 1-2 seen by the referece system 0
-    w23_0 = M02*Lrz*qp(3)*inv(M02);     %speed in 2-3 seen by the referece system 0
-    
-    w02_0 = w01_0 + w12_0;      %speed in 0-2 seen by the referece system 0 (sum of speed 0-1 % 1-2)
-    w03_0 = w02_0 + w23_0;      %speed in 0-3 (gripper total speed) seen by the referece system 0 (sum of speed 0-2 % 2-3)
-    
-    sp = w03_0*s;       % linear speed of gripper is angular velocity multiplied by s.
-    Sp = [Sp sp];
-    
-    
-    % ------------ ACCELERATION EVALUATION -------------
-    
-    h01_0 = (Lrz*qpp(1) + Lrz^2*qp(1)^2);       %acceleration in 0-1 seen by the referece system 0
-    h12_0 = M01*(Lry*qpp(2) + Lry^2*qp(2)^2)*inv(M01);      %acceleration in 1-2 seen by the referece system 0
-    h23_0 = M02*(Lrz*qpp(3) + Lrz^2*qp(3)^2)*inv(M02);      %acceleration in 2-3 seen by the referece system 0
-    
-    h02_0 = h01_0 + h12_0 + 2*w01_0*w12_0;      %acc in 0-2 seen by the referece system 0 (sum of acc 0-1 % 1-2 and coriolis between them)
-    h03_0 = h02_0 + h23_0 + 2*w02_0*w23_0;      %acc in 0-3 seen by the referece system 0 (sum of acc 0-2 % 2-3 and coriolis between them)
-    
-    spp = h03_0*s;
-    Spp = [Spp spp];
-
-
-end
-
-
-
-%% Transizione Velocità da cartesiano a giunti angolari
-
-Nmax = 1000;     % numero massimo iterazioni Newton
-toll = 1e-10;    % tolleranza Newton
-
-Q_inv = [];
-Qp_inv = [];
-Qpp_inv = [];
-
-% faccio la prima ricerca fuori in modo da partire con il punto corretto
-[q_inv,iter] = newtonRaphson(Si',Qini_Inv',L,toll,Nmax);
-disp(iter)
-Q_inv = [Q_inv q_inv];
-Qp_inv = [0;0;0];
-Qpp_inv = [0;0;0];
-
-[x,xp,xpp,tt] = cycloidValues(T,n,Si(1),dS(1));
-[y,yp,ypp,tt] = cycloidValues(T,n,Si(2),dS(2));
-[z,zp,zpp,tt] = cycloidValues(T,n,Si(3),dS(3));
-
-
-for i = 2:n
-
-
-    % ---------------------------------------------------------------------
-    % ----------------------- INVERSE KINEMATICS --------------------------
-    
-    
-    s_inv = [x(i-1);y(i-1);z(i-1)];
-    
-    [q_inv,iter] = newtonRaphson(s_inv,Q_inv(:,i-1),L,toll,Nmax);
-    Q_inv = [Q_inv q_inv];
-    
-
-%     disp(i)
-%     if iter >= Nmax
-%         disp("errore")
-%     end
-
-
-    j = Jac_AAA(q_inv,L);
-    sp_inv = [xp(i);yp(i);zp(i)];
-    jinv = (j^-1);
-    qp_inv = jinv*sp_inv;
-    Qp_inv = [Qp_inv qp_inv];
-
-    
-    jp = JacP_AAA(q_inv,qp_inv,L);
-    spp_inv = [xpp(i);ypp(i);zpp(i)];
-    qpp_inv = jinv*(spp_inv-jp*qp_inv);
-    Qpp_inv = [Qpp_inv qpp_inv];
-    
-    
-end
-
-
-%% plot configurations
-
-% Dati da cinematica diretta:
-Plot_Trajectory_AAA(Qi,Qf,L,S(1,:),S(2,:),S(3,:),n,"Giunti");
-
-% Dati da cinematica inversa:
-Plot_Trajectory_AAA(Q_inv(:,1),Q_inv(:,end),L,x,y,z,n,"Lineare");
-
-
-%% plot graph of data x,y,z position speed and acceleration
-
-Plot_Graphs_Dir_Kinematics_AAA(S,Sp,Spp,tt,dT)
-
-
-%% plot graph of data a,b,c position speed and acceleration
-
-Plot_Graphs_Inv_Kinematics_AAA(Q_inv,Qp_inv,Qpp_inv,tt,dT)
-
-
-
+%% ------------------------------------------------------------------------------
+%  ------------------------------ DYNAMICS DATA ---------------------------------
 %  ------------------------------------------------------------------------------
-%% --------------------------------- DYNAMICS -----------------------------------
-%  ------------------------------------------------------------------------------
-
 
 
 %-------- external forces ----------
@@ -277,7 +134,14 @@ J33 = M3_3*J3_3*M3_3';
 
 
 
-%% Dynamic Evaluation
+%% Transizione Velocità da giunti angolari a cartesiano
+
+S = [];
+Sp = [];
+Spp = [];
+
+
+% Dynamic Evaluation
 
 J11_0 = [];
 J22_0 = [];
@@ -304,17 +168,44 @@ WW_tot = [];
 
 
 
+
 for i = 1:n
-   
-    % ------------ POSITION EVALUATION -------------
-         
-    [s,M01,M12,M23] = Rotation_Matrixes_AAA(Q_inv(:,i),L); 
+    
+    
+    % ---------------------------------------------------------------------
+    % ----------------------- DIRECT KINEMATICS ---------------------------
+    % ---------------------------------------------------------------------
+
+    
+    t=(i-1)*T/(n-1);      % time from 0 to T with step dT
+    tt(i)=t;
+    [a(i),ap(i),app(i)] = cycloidLaw(t,T,Qi(1),dQ(1));
+    [b(i),bp(i),bpp(i)] = cycloidLaw(t,T,Qi(2),dQ(2));
+    [c(i),cp(i),cpp(i)] = cycloidLaw(t,T,Qi(3),dQ(3));
+    
+    q = [a(i);b(i);c(i)];
+    qp = [ap(i);bp(i);cp(i)];
+    qpp = [app(i);bpp(i);cpp(i)];
+    
+    [s,M01,M12,M23] = Rotation_Matrixes_AAA(q,L); 
+    
+    S = [S s];
     
     M02 = M01*M12;
     M03 = M02*M23;
     
     
-     % ------------ SPEED EVALUATION -------------
+    % -------------------------------------IMPORTANTE--------------------------------
+    % -------------------------------------------------------------------------------
+    
+    % In generale Hij_k significa accelerazione di (j) rispetto ad (i) vista dal sistema 
+    % (k)  (in maniera analoga anche per le velocità)
+    
+    % -------------------------------------IMPORTANTE--------------------------------
+    % -------------------------------------------------------------------------------
+    
+    
+    % ------------ SPEED EVALUATION -------------
     
     w01_0 = Lrz*qp(1);      %speed in 0-1 seen by the referece system 0
     w12_0 = M01*Lry*qp(2)*inv(M01);     %speed in 1-2 seen by the referece system 0
@@ -322,6 +213,9 @@ for i = 1:n
     
     w02_0 = w01_0 + w12_0;      %speed in 0-2 seen by the referece system 0 (sum of speed 0-1 % 1-2)
     w03_0 = w02_0 + w23_0;      %speed in 0-3 (gripper total speed) seen by the referece system 0 (sum of speed 0-2 % 2-3)
+    
+    sp = w03_0*s;       % linear speed of gripper is angular velocity multiplied by s.
+    Sp = [Sp sp];
     
     
     % ------------ ACCELERATION EVALUATION -------------
@@ -333,9 +227,17 @@ for i = 1:n
     h02_0 = h01_0 + h12_0 + 2*w01_0*w12_0;      %acc in 0-2 seen by the referece system 0 (sum of acc 0-1 % 1-2 and coriolis between them)
     h03_0 = h02_0 + h23_0 + 2*w02_0*w23_0;      %acc in 0-3 seen by the referece system 0 (sum of acc 0-2 % 2-3 and coriolis between them)
     
-    
-    % ------------ DYNAMIC EVALUATION -------------
-    
+    spp = h03_0*s;
+    Spp = [Spp spp];
+
+
+
+
+    % ---------------------------------------------------------------------
+    % ----------------------- DYNAMICS EVALUATION -------------------------
+    % ---------------------------------------------------------------------
+
+
     j11_0 = M01*J11*M01';
     j22_0 = M02*J22*M02';
     j33_0 = M03*J33*M03';
@@ -356,11 +258,12 @@ for i = 1:n
     
     F_ext_0 = [F_ext_0 fext_0];
     
+
     %----------------- KINETIC ENERGY -------------------
     
-    ek1 = trace(1/2*w01_0*J11*w01_0');
-    ek2 = trace(1/2*w02_0*J22*w02_0');
-    ek3 = trace(1/2*w03_0*J33*w03_0');
+    ek1 = trace(1/2*w01_0*j11_0*w01_0');
+    ek2 = trace(1/2*w02_0*j22_0*w02_0');
+    ek3 = trace(1/2*w03_0*j33_0*w03_0');
     
     ek_tot = ek1 + ek2 + ek3;
     
@@ -369,9 +272,9 @@ for i = 1:n
     
     %----------------- POTENTIAL ENERGY ------------------- 
     
-    ep1 = -trace(Hg*J11);
-    ep2 = -trace(Hg*J22);
-    ep3 = -trace(Hg*J33);
+    ep1 = -trace(Hg*j11_0);
+    ep2 = -trace(Hg*j22_0);
+    ep3 = -trace(Hg*j33_0);
 
     ep_tot = ep1 + ep2 + ep3;
     
@@ -380,9 +283,9 @@ for i = 1:n
     
     %----------------- TORQUE OF FORCES -------------------
     
-    cc1 = -PseDot(FF1,Lrz);
-    cc2 = -PseDot(FF2,Lry);
-    cc3 = -PseDot(FF3,Lrz);
+    cc1 = -PseDot(ff1,Lrz);
+    cc2 = -PseDot(ff2,Lry);
+    cc3 = -PseDot(ff3,Lrz);
     
     CC1 = [CC1 cc1];
     CC2 = [CC2 cc2];
@@ -391,9 +294,9 @@ for i = 1:n
     
     %----------------- POWER OF FORCES -------------------
     
-    ww1 = -PseDot(FF3,w23_0);
-    ww2 = -PseDot(FF2,w12_0);
-    ww3 = -PseDot(FF1,w01_0);
+    ww1 = -PseDot(ff1,w01_0);
+    ww2 = -PseDot(ff2,w12_0);
+    ww3 = -PseDot(ff3,w23_0);
     ww4 = PseDot(fext_0,w03_0);
     ww_tot = ww1+ww2+ww3+ww4;
     
@@ -402,9 +305,171 @@ for i = 1:n
     WW3 = [WW3 ww3];
     WW4 = [WW4 ww4];
     WW_tot = [WW_tot ww_tot];
+
+
+end
+
+E_tot = EK_tot + EP_tot;    % energia totale
+
+P_tot = theorethicalDiff(E_tot,dT);     % total theoretical diff of power
+
+
+
+
+
+%% ------------------------------------------------------------------------
+%  ------------------------ INVERSE KINEMATICS ----------------------------
+%  ------------------------------------------------------------------------
+
+
+% Transizione Velocità da cartesiano a giunti angolari
+
+Nmax = 1000;     % numero massimo iterazioni Newton
+toll = 1e-10;    % tolleranza Newton
+
+Q_inv = [];
+Qp_inv = [];
+Qpp_inv = [];
+
+% faccio la prima ricerca fuori in modo da partire con il punto corretto
+[q_inv,iter] = newtonRaphson(Si',Qini_Inv',L,toll,Nmax);
+disp(iter)
+Q_inv = [Q_inv q_inv];
+Qp_inv = [0;0;0];
+Qpp_inv = [0;0;0];
+
+[x,xp,xpp,tt] = cycloidValues(T,n,Si(1),dS(1));
+[y,yp,ypp,tt] = cycloidValues(T,n,Si(2),dS(2));
+[z,zp,zpp,tt] = cycloidValues(T,n,Si(3),dS(3));
+
+
+for i = 2:n
+
+
+    % ---------------------------------------------------------------------
+    % ----------------------- INVERSE KINEMATICS --------------------------
+    % ---------------------------------------------------------------------
+
+
+    s_inv = [x(i-1);y(i-1);z(i-1)];
+    
+    [q_inv,iter] = newtonRaphson(s_inv,Q_inv(:,i-1),L,toll,Nmax);
+    Q_inv = [Q_inv q_inv];
+    
+
+%     disp(i)
+%     if iter >= Nmax
+%         disp("errore")
+%     end
+
+
+    j = Jac_AAA(q_inv,L);
+    sp_inv = [xp(i);yp(i);zp(i)];
+    jinv = (j^-1);
+    qp_inv = jinv*sp_inv;
+    Qp_inv = [Qp_inv qp_inv];
+
+    
+    jp = JacP_AAA(q_inv,qp_inv,L);
+    spp_inv = [xpp(i);ypp(i);zpp(i)];
+    qpp_inv = jinv*(spp_inv-jp*qp_inv);
+    Qpp_inv = [Qpp_inv qpp_inv];
     
     
 end
 
 
 
+%% plot configurations
+
+% Dati da cinematica diretta:
+Plot_Trajectory_AAA(Qi,Qf,L,S(1,:),S(2,:),S(3,:),n,"Giunti");
+
+% Dati da cinematica inversa:
+Plot_Trajectory_AAA(Q_inv(:,1),Q_inv(:,end),L,x,y,z,n,"Lineare");
+
+
+%% plot graph of data x,y,z position speed and acceleration
+
+Plot_Graphs_Dir_Kinematics_AAA(S,Sp,Spp,tt,dT)
+
+
+%% plot graph of data a,b,c position speed and acceleration
+
+Plot_Graphs_Inv_Kinematics_AAA(Q_inv,Qp_inv,Qpp_inv,tt,dT)
+
+
+
+%% Plot Coppie
+
+% --------- torques ------------
+figure
+
+subplot(3,1,1)
+plot(tt,CC1,'b')
+legend("Fq 1")
+grid on
+title("Torques")
+
+subplot(3,1,2)
+plot(tt,CC2,'g')
+legend("Fq 2")
+grid on
+
+subplot(3,1,3)
+plot(tt,CC3,'r')
+legend("Fq 3")
+grid on
+
+
+%% Plot Energia
+
+% --------- energy ------------
+figure
+
+subplot(3,1,1)
+plot(tt,EK_tot,'b')
+legend("kinetic energy")
+grid on
+title("Energy")
+
+subplot(3,1,2)
+plot(tt,EP_tot,'g')
+legend("potential energy")
+grid on
+
+subplot(3,1,3)
+plot(tt,E_tot,'r')
+legend("total energy")
+grid on
+
+
+%% Plot Potenze reali e teoriche
+
+% --------- power ------------
+figure
+
+subplot(3,1,1)
+plot(tt,EK_tot,'b')
+legend("kinetic energy")
+grid on
+title("Energy")
+
+subplot(3,1,2)
+plot(tt,EP_tot,'g')
+legend("potential energy")
+grid on
+
+subplot(3,1,3)
+plot(tt,E_tot,'r')
+legend("total energy")
+grid on
+
+
+figure
+hold on
+grid on
+plot(tt,WW_tot,'b')
+plot(tt(1:length(P_tot)),P_tot,'r')
+legend("Real power", "Theoric power")
+title("power in horizontal plane")
