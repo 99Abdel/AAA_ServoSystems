f7346dc3c271a08bd153511eae9e85e464f1be3b
99Abdel <a.msaad@studenti.unibs.it>
2022-05-22 12:14:17 +0200

    ColdFix pianoInclinato

--- PianoInclinato.m
+++ PianoInclinato.m
@@ -5,9 +5,9 @@ function [N01,N02,N03,N12,N23] = PianoInclinato(S,gamma)
     yp2 = S(2,end);
     zp2 = S(3,end);
     
-    N01 = [1,            0,             0,   xp2;
-           0,   cos(gamma),   -sin(gamma),   yp2;
-           0,   sin(gamma),    cos(gamma),   zp2;
+    N01 = [1,            0,             0,   -xp2;
+           0,   cos(gamma),   -sin(gamma),   -yp2;
+           0,   sin(gamma),    cos(gamma),   -zp2;
            0,            0,             0,     1];
     
     N12 = [ cos(gamma),   0,    sin(gamma),   0;
